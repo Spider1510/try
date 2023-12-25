c6fa7c9a166720bd49ee5109616e1ba5fa210c48
Spider1510 <ameyk1510@gmail.com>
2023-12-25 12:53:19 +0530

    Third commit

--- foo.py
+++ foo.py
@@ -1,11 +1,15 @@
 def args_try(main_arg, *args, **kwargs):
+    print()
     print(f"main arg : {main_arg}")
+    print()
     print(f"var args : ")
     for i, j in enumerate(args, 0):
         print(f"i : {i}\t j : {j}")
+    print()
     print(f"key word args : ")
     for key, item in kwargs.items():
         print(f"Key : {key}\t item : {item}")
+    print()
 
 print("Hello!")
 args_try("foo", "bar1", "bar2", "bar3", pos1=0, pos2="1", pos3=2.0, pos4="three")
